cff77dfa27f8bde9b81de9fd35f8900cca58442e
wisemankaitlyn <68436733+wisemankaitlyn@users.noreply.github.com>
2020-10-22 21:28:13 -0400

    complete interim

---
+++ Gate.cpp
@@ -0,0 +1,12 @@
+/*
+UNNECESSARY
+Gate.cpp  Implementation of Gate class.
+Author:   Kaitlyn Wiseman
+Modified: 22 Oct 2020
+*/
+
+#include<iostream>
+#include "Gate.h"
+#include "Wire.h"
+
+using namespace std;
--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,14 @@
+/*
+Main.cpp  Just a little something to work out what the classes
+			need to do
+Author:   Kaitlyn Wiseman
+Modified: 22 Oct 2020
+*/
+
 #include<iostream>
 #include<fstream>
 #include<vector>
-#include "Wire.h"
+#include "Wire.cpp"
 #include "Gate.h"
 
 int main()
@@ -10,18 +17,33 @@ int main()
 
 	// find the files, output an error if they can't be found.
 
-	string input;
+	string input1;
+	int input2;
 	vector<Wire*> inputs;
 	vector<Wire*> outputs;
 	vector<Wire*> others;
 	vector<Gate*> gates;
 	
+	cin >> input1;
 	while (/*!eof*/)
 	{
-		cin >> input;
-		if (input == "INPUT")
+		if (input1 == "INPUT")
 		{
-			Wire* temp(cin>>)
+			cin >> input1;
+			cin >> input2;
+			Wire temp(input1, input2, 1);
+			inputs.push_back(&temp);
+		}
+		else if (input1 == "OUTPUT")
+		{
+			cin >> input1;
+			cin >> input2;
+			Wire temp(input1, input2, 0);
+			outputs.push_back(&temp);
+		}
+		else
+		{
+
 		}
 	}
 }
